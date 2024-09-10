110be846ab489fecc094d226341f041ebe3dd4a5
23DP1NKris <166118273+23DP1NKris@users.noreply.github.com>
2024-09-10 08:24:24 +0000

    scanner

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,17 +1,26 @@
 package lv.rvt;
+import java.util.Scanner;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
-        // Uzdevuma nosaukums
-        
-        String product = "Book";
-        int amount = 16;
-        boolean confirmed = true;
-        
-        System.out.print(product);
+    public static void main(String[] args)  {
+        // void - neko neatgriež
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the name of your product: ");
+        String product = scanner.nextLine();
+
+        System.out.println("Write the amount of stock: ");
+        int amount = Integer.valueOf(scanner.nextLine());   
+
+        System.out.println("Is the for sale? (true/false): ");
+        boolean confirmed = Boolean.valueOf(scanner.nextLine());
+        
+        System.out.println("Name of the product: " + product);
+        System.out.println("Amount left: " + amount);
+        System.out.println("Avaliable: " + confirmed);
+
+        scanner.close();
     }
-
 }
