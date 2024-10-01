b12c2815e0af94d67ea43132b472e3ec80b3db31
23DP1NKris <166118273+23DP1NKris@users.noreply.github.com>
2024-10-01 09:28:54 +0000

    Divisible by three

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,13 +1,28 @@
 package lv.rvt;
+import java.util.Scanner;
 
 public class App {
     
     public static void main(String[] args)  {
-        printUntilNumber(5);
+        Scanner scanner = new Scanner(System.in);
+        
+        // printUntilNumber(5);
+        divisibleByThreeInRange(2, 10);
+
+        scanner.close();
     }
+
     public static void printUntilNumber(int num)  {
         for(int i = 1; i <= num; i++) {
             System.out.println(i);
+        }
+    }
+
+    public static void divisibleByThreeInRange(int start, int end) {
+        for (int i = start; i < end; i++) {
+            if (i % 3 == 0) {
+                System.out.println(i);
+            }
         }
     }
 }
