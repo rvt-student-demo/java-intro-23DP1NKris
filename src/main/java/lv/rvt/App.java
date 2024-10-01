44436704af9606d0998d163f3eaa401ef30a0ba7
23DP1NKris <166118273+23DP1NKris@users.noreply.github.com>
2024-10-01 05:46:21 +0000

    Counting to hundred

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,25 +1,21 @@
 package lv.rvt;
+
 import java.util.Scanner;
 
-public class App 
-{
-    public static void main(String[] args)  {
+public class App
+    {
+    public static void main( String[] args )
+    {
         Scanner scanner = new Scanner(System.in);
 
-        int count = 0;
-        int sum = 0;
+        System.out.println("Enter the start number:");
+        int start = Integer.valueOf(scanner.nextLine());
 
-        while (true) {
-            System.out.println("Give a number:");
-            int number = Integer.valueOf(scanner.nextLine());
-            if (number == 0) {
-                break;
-            }
-            count += 1;
-            sum += number;
+        for(int i = start; i <=100; i++) {
+        System.out.println(i);
         }
 
-        System.out.println("Number of numbers: " + count);
-        System.out.println("Sum of the numbers: " + sum);
+    scanner.close();
+
     }
 }
