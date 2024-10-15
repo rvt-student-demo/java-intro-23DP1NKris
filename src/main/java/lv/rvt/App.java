bf407728a630f49839b10a731d8ae3973811c6d1
23DP1NKris <166118273+23DP1NKris@users.noreply.github.com>
2024-10-15 08:05:41 +0000

    Third element (Fixed)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,11 +4,20 @@ import java.util.*;
 public class App {
     
     public static void main(String[] args)  {
-        ArrayList<String> names = new ArrayList<>(Arrays.asList("Tom", "Emma", "Alex", "Mary"));
-        System.out.println(names.get(2));
-
-        ArrayList<String> names2 = new ArrayList<>(Arrays.asList("Emma", "Alex", "Mary"));
-        System.out.println(names2.get(2));
+        Scanner scanner = new Scanner(System.in);
         
+        ArrayList<String> names = new ArrayList<>();
+        String nameInput = "";
+
+        while (true) {
+            nameInput = scanner.nextLine();
+            if (nameInput.equals("")) {
+                break;
+            }
+
+            names.add(nameInput);
+        }
+        System.out.println(names.get(2));
+        scanner.close();
     }
 }
