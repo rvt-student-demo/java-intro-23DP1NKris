944a4339e54afbf416643d8c598c2643286a1a23
23DP1NKris <166118273+23DP1NKris@users.noreply.github.com>
2024-10-22 06:44:31 +0000

    Only these numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,28 +2,35 @@ package lv.rvt;
 import java.util.*;
 
 public class App {
-    
+
     public static void main(String[] args)  {
         Scanner scanner = new Scanner(System.in);
-        
+
         ArrayList<Integer> numbers = new ArrayList<>();
         int numberInput;
 
         while (true) {
             numberInput = scanner.nextInt();
-            if (numberInput == 0) {
+            if (numberInput == -1) {
                 break;
             }
-
             numbers.add(numberInput);
         }
-        
-        int sum = 0;
-        for (int i = 0; i < numbers.size(); i++) {
-            sum += numbers.get(i);
-        }
 
-        System.out.println(sum);
+        while (true) {
+            System.out.println("From where?");
+            Integer start = scanner.nextInt();
+            System.out.println("To where?");
+            Integer end = scanner.nextInt();
+            if (start < 0 || end > numbers.size() || start >= end) {
+                System.out.println("Invalid input, input the values again");
+            } else {
+                for (int i = start; i < end; i++) {
+                    System.out.println(numbers.get(i));
+                }
+                break;
+            }
+        }
 
         scanner.close();
     }
