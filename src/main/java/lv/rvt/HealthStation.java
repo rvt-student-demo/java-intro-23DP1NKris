256ae7455aff2000af14b02f4a1de33644cc1139
23DP1NKris <166118273+23DP1NKris@users.noreply.github.com>
2024-12-10 10:36:27 +0000

    Health station (part 3)

--- src/main/java/lv/rvt/HealthStation.java
+++ src/main/java/lv/rvt/HealthStation.java
@@ -2,11 +2,19 @@ package lv.rvt;
 
 public class HealthStation {
 
+    private int weighings;
+
     public int weigh(Person person) {
+        weighings++;
         return person.getWeight();
     }
 
     public void feed(Person person) {
         person.setWeight(person.getWeight() + 1); 
     }
+
+    public int weighings() {
+        return weighings;
+    }
+
 }
