6e612d8ce27377726b71d2d1fcd5780c7acf721f
23DP1NKris <166118273+23DP1NKris@users.noreply.github.com>
2025-01-30 14:18:29 +0000

    Warehousing (part 4)

--- src/main/java/lv/rvt/ChangeHistory.java
+++ src/main/java/lv/rvt/ChangeHistory.java
@@ -17,6 +17,34 @@ public class ChangeHistory {
         history.clear();
     }
 
+    public double maxValue() {
+        double max = history.get(0);
+        for (double value : history) {
+            if (value > max) {
+                max = value;
+            }
+        }
+        return max;
+    }
+
+    public double minValue() {
+        double min = history.get(0);
+        for (double value : history) {
+            if (value > min) {
+                min = value;
+            }
+        }
+        return min;
+    }
+
+    public double average() {
+        double sum = 0;
+        for (double value : history) {
+            sum += value;
+        }
+        return sum / history.size();
+    }
+
     @Override
     public String toString() {
         return history.toString();
